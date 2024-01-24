64a6a8aece5ef11f089246cee626c69a2bac8265
halurabiyyu <halurabiyyu@gmail.com>
2024-01-24 21:41:34 +0700

    Change Title

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to Halur's Porto📃</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Welcome to Halur's Porto
         </p>
       </div>
       <p className="read-the-docs">
